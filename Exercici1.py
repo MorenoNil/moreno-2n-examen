fdcfb775cce64166dd60d77f77c7e77153b4bcb1
MorenoNil <nilmoreno2006@ginebro.cat>
2024-11-22 17:57:04 +0100

    commit de les 17:55

---
+++ Exercici1.py
@@ -0,0 +1,25 @@
+if __name__ == '__main__':
+
+    hores = int()
+    minuts = int()
+    totalMinuts = int()
+    horesAminuts = int()
+
+    print("Quantes hores has treballat: ",end="")
+    hores = int(input())
+
+    print("Quantes minuts has treballat: ",end="")
+    minuts = int(input())
+
+    while not hores or minuts < 0:
+
+        print("ERROR \n Torna a entrar les hores: ",end="")
+        hores = int(input())
+
+        print("Quants minuts has treballat: ",end="")
+        minuts = int(input())
+
+
+    horesAminuts = (hores / 60)
+
+    totalMinuts =
